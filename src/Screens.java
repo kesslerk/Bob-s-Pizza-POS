5fff718b52902398ab00d24fc16caeef85ddebf5
kesslerk <kskessler@smcm.edu>
2016-12-01 23:36:52 -0500

    Add files via upload

--- src/Screens.java
+++ src/Screens.java
@@ -595,15 +595,24 @@ public class Screens extends JFrame {
 		 prices.setLayout(null);
 
 			JButton btnSignOut = new JButton("Sign Out");
+			btnSignOut.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					card.show(panel, "login");
+					
+				}
+			});
 			btnSignOut.setBounds(6, 6, 117, 29);
 			prices.add(btnSignOut);
-			btnSignOut.setActionCommand("signout");
 
 			JButton btnOrderScreen = new JButton("Order Screen");
+			btnOrderScreen.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					card.show(panel, "order");
+				}
+			});
 			btnOrderScreen.setBounds(577, 6, 117, 29);
 			prices.add(btnOrderScreen);
-			btnOrderScreen.setActionCommand("orderscreen");
-
+			
 			JLabel lblPizzaPrice = new JLabel("Pizza Price");
 			lblPizzaPrice.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
 			lblPizzaPrice.setBounds(6, 65, 97, 16);
@@ -695,22 +704,79 @@ public class Screens extends JFrame {
 			prices.add(tax);
 
 			JButton btnClear = new JButton("Clear");
+			btnClear.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					pizzaSmall.setText("");
+					pizzaMedium.setText("");
+					pizzaLarge.setText("");
+					toppingSmall.setText("");
+					toppingMedium.setText("");
+					toppingLarge.setText("");
+					sodaPrice.setText("");
+					tax.setText("");
+				}
+			});
 			btnClear.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 			btnClear.setBounds(145, 425, 117, 29);
 			prices.add(btnClear);
-			btnClear.setActionCommand("clear");
 
 			JButton btnApplyChanges = new JButton("Apply Changes");
+			btnApplyChanges.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if ("apply".equals(e.getActionCommand())) {
+						
+						BufferedReader br;
+						try {
+							br = new BufferedReader(new FileReader("Prices"));
+						
+				        String line = br.readLine();
+				 
+				        while(line!=null){
+				            String[] prices = line.split(",");
+				            line=br.readLine();
+				            FileWriter writer = new FileWriter("Prices");
+						
+						if(Float.valueOf(pizzaSmall.getText()) > 0 && !pizzaSmall.getText().isEmpty() && prices[0].equals("pizzaSmall")){
+				             writer.write(prices[1].replace( prices[1],pizzaSmall.getText()));
+						}
+						if(Float.valueOf(pizzaMedium.getText()) > 0 && !pizzaMedium.getText().isEmpty() && prices[0].equals("pizzaMedium")){
+							 writer.write(prices[1].replace( prices[1],pizzaMedium.getText()));
+						}
+						if(Float.valueOf(pizzaLarge.getText()) > 0 && !pizzaLarge.getText().isEmpty() && prices[0].equals("pizzaLarge")){
+							 writer.write(prices[1].replace( prices[1],pizzaLarge.getText()));
+						}
+						if(Float.valueOf(toppingSmall.getText()) > 0 && !toppingSmall.getText().isEmpty() && prices[0].equals("toppingSmall")){
+							 writer.write(prices[1].replace( prices[1],toppingSmall.getText()));
+						}
+						if(Float.valueOf(toppingMedium.getText()) > 0 && !toppingMedium.getText().isEmpty() && prices[0].equals("toppingMedium")){
+							 writer.write(prices[1].replace( prices[1],toppingMedium.getText()));
+						}
+						if(Float.valueOf(toppingLarge.getText()) > 0 && !toppingLarge.getText().isEmpty() && prices[0].equals("toppingLarge")){
+							 writer.write(prices[1].replace( prices[1],toppingLarge.getText()));
+						}
+						if(Float.valueOf(sodaPrice.getText()) > 0 && !sodaPrice.getText().isEmpty() && prices[0].equals("sodaPrice")){
+							 writer.write(prices[1].replace( prices[1],sodaPrice.getText()));
+						}
+						if(Float.valueOf(tax.getText()) > 0 && !tax.getText().isEmpty() && prices[0].equals("tax")){
+							 writer.write(prices[1].replace( prices[1],tax.getText()));
+						}
+						 writer.close();
+			 }
+				        
+						} catch (FileNotFoundException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+					
+				}
+			});
 			btnApplyChanges.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 			btnApplyChanges.setBounds(328, 426, 168, 29);
 			prices.add(btnApplyChanges);
-			btnApplyChanges.setActionCommand("apply");
-
-			// Action Listeners
-			btnSignOut.addActionListener(this);
-			btnOrderScreen.addActionListener(this);
-			btnClear.addActionListener(this);
-			btnApplyChanges.addActionListener(this);
 			
 			//autofill with default prices
 			BufferedReader br;
@@ -720,24 +786,24 @@ public class Screens extends JFrame {
 	        String line = br.readLine();
 	 
 	        while(line!=null){
-	            String[] prices = line.split(",");
+	            String[] priceArray = line.split(",");
 	            line=br.readLine();
-	            if(prices[0].equals("pizzaSmall")){
-	            	pizzaSmall.setText(prices[1]);
-	            }else if(prices[0].equals("pizzaMedium")){
-	            	pizzaMedium.setText(prices[1]);
-	            }else if(prices[0].equals("pizzaLarge")){
-	            	pizzaLarge.setText(prices[1]);
-	            }else if(prices[0].equals("toppingSmall")){
-	            	toppingSmall.setText(prices[1]);
-	            }else if(prices[0].equals("toppingMedium")){
-	            	toppingMedium.setText(prices[1]);
-	            }else if(prices[0].equals("toppingLarge")){
-	            	toppingLarge.setText(prices[1]);
-	            }else if(prices[0].equals("sodaPrice")){
-	            	sodaPrice.setText(prices[1]);
-	            }else if(prices[0].equals("tax")){
-	            	tax.setText(prices[1]);
+	            if(priceArray[0].equals("pizzaSmall")){
+	            	pizzaSmall.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("pizzaMedium")){
+	            	pizzaMedium.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("pizzaLarge")){
+	            	pizzaLarge.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("toppingSmall")){
+	            	toppingSmall.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("toppingMedium")){
+	            	toppingMedium.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("toppingLarge")){
+	            	toppingLarge.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("sodaPrice")){
+	            	sodaPrice.setText(priceArray[1]);
+	            }else if(priceArray[0].equals("tax")){
+	            	tax.setText(priceArray[1]);
 	            }
 	        }
 	        
@@ -749,68 +815,6 @@ public class Screens extends JFrame {
 				e1.printStackTrace();
 			}
 		}
-
-		public void actionPerformed(ActionEvent e) {
-			if ("clear".equals(e.getActionCommand())) {
-				pizzaSmall.setText("");
-				pizzaMedium.setText("");
-				pizzaLarge.setText("");
-				toppingSmall.setText("");
-				toppingMedium.setText("");
-				toppingLarge.setText("");
-				sodaPrice.setText("");
-				tax.setText("");
-			}
-			if ("apply".equals(e.getActionCommand())) {
-				
-				BufferedReader br;
-				try {
-					br = new BufferedReader(new FileReader("Prices"));
-				
-		        String line = br.readLine();
-		 
-		        while(line!=null){
-		            String[] prices = line.split(",");
-		            line=br.readLine();
-		            FileWriter writer = new FileWriter("Prices");
-				
-				if(Float.valueOf(pizzaSmall.getText()) > 0 && !pizzaSmall.getText().isEmpty() && prices[0].equals("pizzaSmall")){
-		             writer.write(prices[1].replace( prices[1],pizzaSmall.getText()));
-				}
-				if(Float.valueOf(pizzaMedium.getText()) > 0 && !pizzaMedium.getText().isEmpty() && prices[0].equals("pizzaMedium")){
-					 writer.write(prices[1].replace( prices[1],pizzaMedium.getText()));
-				}
-				if(Float.valueOf(pizzaLarge.getText()) > 0 && !pizzaLarge.getText().isEmpty() && prices[0].equals("pizzaLarge")){
-					 writer.write(prices[1].replace( prices[1],pizzaLarge.getText()));
-				}
-				if(Float.valueOf(toppingSmall.getText()) > 0 && !toppingSmall.getText().isEmpty() && prices[0].equals("toppingSmall")){
-					 writer.write(prices[1].replace( prices[1],toppingSmall.getText()));
-				}
-				if(Float.valueOf(toppingMedium.getText()) > 0 && !toppingMedium.getText().isEmpty() && prices[0].equals("toppingMedium")){
-					 writer.write(prices[1].replace( prices[1],toppingMedium.getText()));
-				}
-				if(Float.valueOf(toppingLarge.getText()) > 0 && !toppingLarge.getText().isEmpty() && prices[0].equals("toppingLarge")){
-					 writer.write(prices[1].replace( prices[1],toppingLarge.getText()));
-				}
-				if(Float.valueOf(sodaPrice.getText()) > 0 && !sodaPrice.getText().isEmpty() && prices[0].equals("sodaPrice")){
-					 writer.write(prices[1].replace( prices[1],sodaPrice.getText()));
-				}
-				if(Float.valueOf(tax.getText()) > 0 && !tax.getText().isEmpty() && prices[0].equals("tax")){
-					 writer.write(prices[1].replace( prices[1],tax.getText()));
-				}
-				 writer.close();
-	 }
-		        
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		
-	 }
 }
 
 	
